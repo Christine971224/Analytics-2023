f15cfee1bbaaa8b2e7c173980788d3c2bcff1c19
Minghua Zhu <jupyter-zhuaaz@tljh-instance.c.tljh-savasnu-391068.internal>
2023-09-03 20:52:31 +0000

    analytics

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The practice of course',
+    author='Minghua Zhu',
+    license='MIT',
+)
